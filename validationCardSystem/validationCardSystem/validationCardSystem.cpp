dfebef009d89cc6f0e3c09390f26d4d45a83c905
assab78 <88334601+assab78@users.noreply.github.com>
2022-11-23 14:40:44 +0200

    v1

---
+++ validationCardSystem/validationCardSystem/validationCardSystem.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string cardNum;
+	string prefix = "4";
+	cout << "Write your card number: ";
+	cin >> cardNum;
+	cout <<endl;
+	x:
+	if (cardNum[0] == '4') {
+		if (cardNum.size()==12)
+		{
+			cout << "your card correct";
+
+		}
+		else
+		{
+			cout << "You write card incorrect,try again :";
+			cin >> cardNum;
+			cout << endl;
+			goto x;
+		}
+	}
+	else
+	{
+		cout << "Card number must start from number 4,try again:";
+		cin >> cardNum;
+		cout << endl;
+		goto x;
+	}
+}
